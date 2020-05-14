680e74655a664286fc8670ad880a0550afac4a2f
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-14 22:33:47 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+ words = ["wow" , "I"] 
+ words.sort!
 end
 
 def using_reverse(array)
-
+  words = ["wow" , "arrays!"]
+  words.reverse!
 end
 
 def using_first(array)
-
+  words = ["wow" , "a" , "xenomorph"]
+  words.first
 end
 
 def using_last(array)
-
+  words = ["wow" , "arrays!"]
+  words.last
 end
 
 def using_size(array)
-
+  cars = ["R34" , "GT500" , "RX7" , "Supra" , "Charger" , "GT3RS"]
+  cars.size
 end
